ed6c6897d4af9bef67ea0a6e3b4e868c728670ff
Enrique Fernandez <efernandez@clearpathrobotics.com>
2015-03-31 16:40:39 -0400

    Install QtCreator automatically

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-./install_debians.sh
+sudo ./install_debians.sh
+
+# Install QtCreator
+# http://www.qt.io/download-open-source/#section-6
+wget http://download.qt.io/official_releases/qtcreator/3.3/3.3.2/qt-creator-opensource-linux-x86_64-3.3.2.run
+chmod +x qt-creator-opensource-linux-x86_64-3.3.2.run
+sudo ./qt-creator-opensource-linux-x86_64-3.3.2.run
+rm qt-creator-opensource-linux-x86_64-3.3.2.run
 
 # Install QtCreator desktop file
-cp qtcreator.desktop /usr/share/applications
+sudo cp qtcreator.desktop /usr/share/applications
 
