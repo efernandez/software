90944f3ad621d93d9d8b7ce7a1cf7bb29b0ee753
Enrique Fernandez <efernandez@clearpathrobotics.com>
2015-04-01 06:49:55 -0400

    Download skype and install it

--- install.sh
+++ install.sh
@@ -12,3 +12,10 @@ rm qt-creator-opensource-linux-x86_64-3.3.2.run
 # Install QtCreator desktop file
 sudo cp qtcreator.desktop /usr/share/applications
 
+# Install skype
+# http://www.skype.com/en/download-skype/skype-for-linux/downloading/?type=ubuntu64
+# NOTE It needs several i386 packages to be installed first!
+wget http://download.skype.com/linux/skype-ubuntu-precise_4.3.0.37-1_i386.deb
+sudo dpkg -i skype-ubuntu-precise_4.3.0.37-1_i386.deb
+rm skype-ubuntu-precise_4.3.0.37-1_i386.deb
+
