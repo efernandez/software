1bfde7aaeae199a8810b2d26e8c47081dd0937b5
Enrique Fernandez <efernandez@clearpathrobotics.com>
2015-04-10 13:47:49 -0400

    Add python bokeh

---
+++ install_python_extras.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# http://bokeh.pydata.org/en/latest/docs/quickstart.html#quickstart
+# @todo bokeh needs numpy >= 1.7.0 (ubuntu12 has 1.6.1)!
+# instructions for installing numpy and scipy:
+# http://www.scipy.org/scipylib/download.html
+sudo pip install bokeh
+
