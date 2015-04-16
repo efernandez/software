736d73d6a3179732fdc17e61b7b3437099bffc74
Enrique Fernandez <efernandez@clearpathrobotics.com>
2015-04-16 11:19:07 -0400

    Install OpenSCAD

--- install_debians.sh
+++ install_debians.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# Repository for OpenSCAD:
+sudo add-apt-repository ppa:openscad/releases
+sudo apt-get update
+
+# Install all debians:
 cat debians.txt | grep -v \# | xargs sudo apt-get install -y
 
