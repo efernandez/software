9100ca9d7a2b75238286bd8a75709b768db49eb1
Enrique Fernandez <efernandez@clearpathrobotics.com>
2015-04-15 20:50:38 -0400

    Fix install vim and make it robust

--- install_vim.sh
+++ install_vim.sh
@@ -4,14 +4,36 @@
 # - clipboard copy&paste: http://vim.wikia.com/wiki/Accessing_the_system_clipboard
 # - YCM (YouCompleteMe) plugin
 
-cd ~/software
+SOFTWARE_FOLDER=~/software
+
+if [[ ! -d $SOFTWARE_FOLDER ]]
+then
+    mkdir -p $SOFTWARE_FOLDER
+fi
+
+OLD_PWD=$PWD
+cd $SOFTWARE_FOLDER
 
 hg clone https://vim.googlecode.com/hg/ vim
 cd vim
 
-./configure --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/configmake -j8
+./configure --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config
+make -j8
 sudo make install
 
 sudo update-alternatives --install /usr/bin/vim vim /usr/local/bin/vim 10
 sudo update-alternatives --set vim /usr/local/bin/vim
+
+# .vimrc
+git clone --recursive git@github.com:efernandez/vim-config.git ~/.vim
+if [[ -f ~/.vimrc ]]
+then
+    mv ~/.vimrc{,.orig}
+fi
+ln -s ~/.vim/vimrc ~/.vimrc
+cd ~/.vim
+git submodule update --init
+vim +BundleInstall
+
+cd $OLD_PWD
 
