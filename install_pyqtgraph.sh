6883207d9fadd8c42e0ec1cd74224423ef8c5692
Enrique Fernandez <efernandez@clearpathrobotics.com>
2015-07-03 12:48:15 -0400

    Add PyQtGraph installer (for rqt_plot)

---
+++ install_pyqtgraph.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget http://www.pyqtgraph.org/downloads/python-pyqtgraph_0.9.10-1_all.deb
+sudo dpkg -i python-pyqtgraph_0.9.10-1_all.deb
+
+rm -rf python-pyqtgraph_0.9.10-1_all.deb
+
