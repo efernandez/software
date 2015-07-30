90aeeb1e14477201a413a7bba21e846460543f6d
Enrique Fernandez <efernandez@clearpathrobotics.com>
2015-07-30 17:16:21 -0400

    Add -y to apt-get install simplescreenrecorder

--- install_simplescreenrecorder.sh
+++ install_simplescreenrecorder.sh
@@ -3,8 +3,8 @@
 # http://www.maartenbaert.be/simplescreenrecorder/#download
 sudo add-apt-repository ppa:maarten-baert/simplescreenrecorder
 sudo apt-get update
-sudo apt-get install simplescreenrecorder
+sudo apt-get install -y simplescreenrecorder
 
 # Record 32-bit OpenGL applications on a 64-bit system:
-sudo apt-get install simplescreenrecorder-lib:i386
+sudo apt-get install -y simplescreenrecorder-lib:i386
 
