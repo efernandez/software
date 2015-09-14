2a8ba24339ff43e5d48bf8394207b1b581eed096
Enrique Fernandez <efernandez@clearpathrobotics.com>
2015-09-14 18:48:55 -0400

    Update install bokeh

--- install_python_extras.sh
+++ install_python_extras.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # http://bokeh.pydata.org/en/latest/docs/quickstart.html#quickstart
-# @todo bokeh needs numpy >= 1.7.0 (ubuntu12 has 1.6.1)!
+# Note that bokeh needs numpy >= 1.7.0 an Ubuntu 112 has 1.6.1, but
+# it's fine with Ubuntu 14
 # instructions for installing numpy and scipy:
 # http://www.scipy.org/scipylib/download.html
 sudo pip install bokeh
