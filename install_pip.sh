f8bb819c9475a6eaf7bcceb49bf8070deaa38687
Enrique Fernandez <efernandez@clearpathrobotics.com>
2015-10-29 14:01:01 -0400

    Fix install_pip

--- install_pip.sh
+++ install_pip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get inall python-pip python-dev build-essential
+sudo apt-get install python-pip python-dev build-essential
 
 sudo pip install --upgrade pip
 sudo pip install --upgrade virtualenv
